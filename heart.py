720cd71e85b2fe0a55c337b578e143fff86f0c0f
miniral199 <miniral199@gmail.com>
2026-01-08 19:38:11 +0300

    absolute change

--- heart.py
+++ heart.py
@@ -30,7 +30,52 @@ class Heart:
         if not self.s=='0':
             self.template=self.__generate_template()
         for line in self.template:
-            print(line)
+            for l in line:
+                print(l*x,end='')
+            print()
+
+def main():
+    # heart=Heart('/')
+    # heart.print_heart()
+    #
+    # heart1=Heart('|')
+    # heart1.print_heart()
+    #
+    # heart2=Heart("\\")
+    # heart2.print_heart(2)
+
+    while True:
+        inp=input("Введите символ из которого хотите себе сердечко (если хотите выйти напишите Exit): ")
+        if inp=='':
+            print("Вы ничего не ввели! Повторите снова!")
+            continue
+
+        elif inp.lower()=='exit':
+            print("Вы выбрали выйти из программы. До свидания, желаю вам всего хорошего и хорошего вам дня!")
+            break
+
+        elif len(inp)!=1:
+            print("Нужно ввести лишь один символ!")
+            continue
+
+        else:
+            heart=Heart(inp)
+            n_try = 1
+            while True:
+                multiplier=input("Введите значение x (строго больше нуля), это будет множителем сердечек: ")
+                if multiplier.isnumeric():
+                    if int(multiplier)>0:
+                        heart.print_heart(int(multiplier))
+                        break
+                    else:
+                        n_try+=1
+                        print(f"Ошибка! Вы ввели: {multiplier}\nМножитель сердечек строго больше нуля! Попытка: #{n_try}")
+
+                else:
+                    n_try+=1
+                    print(f"Ошибка! Вы ввели: {multiplier}\nА надо ввести число или цифру! Попытка: #{n_try}")
+
+
 
 if __name__=="__main__":
     heart=Heart('/')
