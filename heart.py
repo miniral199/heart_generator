d89c331390b0f12ddac573ae90329ed96a3aa6a3
miniral199 <miniral199@gmail.com>
2026-01-08 09:26:07 +0300

    This file creates heart but with bugs

---
+++ heart.py
@@ -0,0 +1,44 @@
+class Heart:
+    def __init__(self,s):
+        #s-template's symbol
+        self.s=s
+        # heart template
+        self.template=[
+            '0   0',
+            '00 00',
+            '00000',
+            ' 000 ',
+            '  0  '
+        ]
+
+    def __generate_template(self):
+       new_template=[]
+       for line in self.template:
+           new_template.append(line.replace('0',self.s))
+
+       return new_template
+
+
+        # for i in range(4):
+        #     line=''
+        #     for j in range(6):
+        #         line+=self.s
+        #     self.template.append(line)
+                
+
+    def print_heart(self):
+        if not self.s=='0':
+            self.template=self.__generate_template()
+        for line in self.template:
+            print(line)
+
+if __name__=="__main__":
+    heart=Heart('/')
+    heart.print_heart()
+
+    heart1=Heart('|')
+    heart1.print_heart()
+
+
+ 
+     
